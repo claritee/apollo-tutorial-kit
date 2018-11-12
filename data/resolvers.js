83856d2dfc4d4b76639e032522932ab8553ff1eb
Claire Tran <c.tran83@gmail.com>
2018-11-12 13:51:04 +1100

    Adding sqlite

---
+++ data/resolvers.js
@@ -0,0 +1,23 @@
+//import { Author, Post } from './connectors';
+
+import { Author } from './connectors';
+
+const resolvers = {
+  Query: {
+    author(_, args) {
+      return Author.find({ where: args });
+    },
+  },
+  Author: {
+    posts(author) {
+      return author.getPosts();
+    },
+  },
+  Post: {
+    author(post) {
+      return post.getAuthor();
+    },
+  },
+};
+
+export default resolvers;
